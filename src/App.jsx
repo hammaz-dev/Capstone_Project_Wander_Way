57d08968184672f08555887595b68f563d321ea7
Mohd Hammaz <mohd.ashad200881@gmail.com>
2026-04-26 11:01:21 +0530

    Fix JSX structure  correctly

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           <Route path="/saved"      element={<SavedTrips />}  />
         </Routes>
       </BrowserRouter>
-      </BrowserRouter>
+      
     </TripProvider>
   )
 }
